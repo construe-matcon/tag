de2c392fde2497186882eac160a4dd50d73ad9cc
Eduardo F. Dutra <eduardo.dutra@voidstdo.com>
2018-12-17 17:22:42 -0200

    Starter Jenkins - DEV

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App - TAG.CONSTRUE.CF"/>
+    <HelloWorld msg="Welcome to Your Vue.js App - TAG-DEV.CONSTRUE.CF"/>
   </div>
 </template>
 
